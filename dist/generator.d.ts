1f4b4c9581c5c86fc3ae6ec9b0cbbdded7ff5aa3
Swetabh Suman <swtbhsmn@gmail.com>
2025-07-06 12:43:36 +0530

    dist-generated

---
+++ dist/generator.d.ts
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+export declare function generateGraphQL(modelFilePath: string, useJS?: boolean): Promise<void>;
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateGraphQL = void 0;
+// FILE: src/index.ts
+var generator_1 = require("./generator");
+Object.defineProperty(exports, "generateGraphQL", { enumerable: true, get: function () { return generator_1.generateGraphQL; } });
+//# sourceMappingURL=index.js.map
